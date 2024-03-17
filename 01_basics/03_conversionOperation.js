006ea58dd6a818856559a65b6e947a99d9df9a9a
Shuaib464 <113933708+Shuaib464@users.noreply.github.com>
2024-03-17 01:15:01 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+///   operation
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+
+console.log(+true);  
+// -->  1
+console.log(+"");     
+//  -->  0
