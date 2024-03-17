88f191bf89e47d717f3230dd2171adc40c5cfd6b
Shuaib464 <113933708+Shuaib464@users.noreply.github.com>
2024-03-17 02:38:51 +0000

    basics about numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,13 @@
+const score = 100     //  NUMBER
+// console.log(typeof score);
+console.log(score);
+
+const balance = new Number(100)    //  OBJECT
+// console.log(typeof balance);
+console.log(balance);
+
+console.log(balance.toString());   //converts in string 
+console.log(balance.toString().length);   //then we can apply string functions on it
+
+
+//  TO BE CONTINUED FROM 7:00 
