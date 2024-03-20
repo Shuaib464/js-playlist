320f57534b890d7597e2ed1dcdfdad645c09b2e3
Shuaib464 <113933708+Shuaib464@users.noreply.github.com>
2024-03-20 05:53:37 +0000

    some imp concepts regarding Math object

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,13 +1,37 @@
 const score = 100     //  NUMBER
 // console.log(typeof score);
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)    //  OBJECT
 // console.log(typeof balance);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());   //converts in string 
-console.log(balance.toString().length);   //then we can apply string functions on it
+// console.log(balance.toString());   //converts in string 
+// console.log(balance.toString().length);   //then we can apply string functions on it
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
 
 
-//  TO BE CONTINUED FROM 7:00 
+// ++++++++++++++++++++++++   MATHS   +++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log("Absolute value-:", Math.abs(-5));
+// console.log("Round val -: ",Math.round(3.6));
+// console.log("Ceiling val -: ",Math.ceil(5.1));
+// console.log("Floor val -: ", Math.floor(8.9));
+// console.log("Square root of 81 -: ",Math.sqrt(81));
+// console.log("Min value in given array - ", Math.min(2,6,8,1,9) );
+// console.log("Max value in given array - ", Math.max(2,6,8,1,9) );
+
+console.log("Random value -: ", Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+//formula to generate random no in a given range
+//  ( Math.random() * (max - min + 1) ) + min
+
+const min = 10
+const max = 25
+
+console.log(Math.floor((Math.random() * ( max - min + 1))) + min );
