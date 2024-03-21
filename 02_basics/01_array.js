17a2a9b46c1198ab71d75c76db1a10fa6d588357
Shuaib464 <113933708+Shuaib464@users.noreply.github.com>
2024-03-21 11:54:40 +0000

    basic methods of array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,14 +1,14 @@
 // Array 
 //defining an array
 
-const myArr = [0,1,2,3,4,];
-console.log(myArr);
+const myArr1 = [0,1,2,3,4,];
+console.log(myArr1);
 
 const myArr2 = new Array(1,2,3,4)
-console.log(myArr2);
+// console.log(myArr2);
 
 const mixedTypeArr = [0,1,2,true, "Shuaib"]
-console.log(mixedTypeArr);
+// console.log(mixedTypeArr);
 
 // NOTE - 1- Array can store multiple type data in js
 //        2- There is no size limitation in array (in js)
@@ -20,3 +20,23 @@ original array
 
 DEEP COPY - it doesn't share the same refence point
 */
+
+
+//  +++++++++++++++++++++++  Array Methods +++++++++++++++++++++++
+const myArr = ["Shuaib", "Newton", "Tofeek", "Aman"]
+
+myArr.push("Ayaan");        // append element at the end
+myArr.pop()                 // remove the last element
+
+// myArr.unshift("Rohaan")     // append at the start 
+// myArr.shift()               // delete at the start
+
+// console.log(myArr);
+
+// console.log(myArr.includes("Tofeek"));    // return boolean (true / false)
+// console.log(myArr.indexOf("Shuaib"));        // return either index or -1(false if not present)
+
+// const newArr = myArr.join()      // converts array into the string and returns it
+// console.log(newArr);
+
+// const myNewArr1 = myArr.slice(1, 3)        // return a copy of a section of array
