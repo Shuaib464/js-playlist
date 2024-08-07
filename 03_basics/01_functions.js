642254e314cdecfd5809df5363f5b53f09c806e9
Shuaib464 <113933708+Shuaib464@users.noreply.github.com>
2024-08-07 02:31:54 +0000

    rest/spread operator in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,11 +6,11 @@
     NOTE -> 1-  when we define a function, then the given input is called "parameters"
             2-  when we call a function, then the given input is called "Arguments"
 */
-function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
-}
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
 
-addTwoNumbers(4, null)
+// addTwoNumbers(4, null)
 
 // giving default value to the function
 function loginUserMsg(userName = "Ahmad"){
@@ -24,4 +24,19 @@ function loginUserMsg(userName = "Ahmad"){
 // console.log(loginUserMsg("Shuaib"));
 console.log(loginUserMsg());
 
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(10,20,30));     // 10
+
+
+/* rest operator in function */
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(100, 200, 300));   // [ 100, 200, 300 ]
+console.log(calculateCartPrice(100, 200, 300, 7000));    // [300, 7000]
 
