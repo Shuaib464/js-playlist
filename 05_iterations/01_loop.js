a6c27e4e15600d9ea9bc4e945842d4ab09004b63
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-12 10:42:28 +0530

    break keyword

--- 05_iterations/01_loop.js
+++ 05_iterations/01_loop.js
@@ -1,6 +1,18 @@
-/* for */
+/* for Loop */
 
 for (let i = 0; i < 10; i++) {
     const element = i;
     console.log(element);
+}
+
+// "break"  and  "continue"
+
+for (let index = 0; index < 20; index++) {
+    
+    if(index == 10){
+        console.log(`detected 10`);
+        break;
+    }
+    console.log(`value of i is ${index}`);
+
 }
