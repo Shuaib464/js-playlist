a9b54deb9343312acf11bff8c1c4381aeea9fff4
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-12 10:59:47 +0530

    "for_of" loop intro

---
+++ 05_iterations/02_array_loops.js
@@ -0,0 +1,16 @@
+/* "for of"  loop 
+
+NOte -> it iterate each element in array
+
+*/
+
+const arr = [1,2,3,4,5]
+
+// for (const e of arr) {
+//     console.log(e);
+// }
+
+const user = "shuaib ahamad";
+for (const e of user) {
+    console.log(`Each character is ${e}`);
+}
