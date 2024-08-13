9967350c59aab2e2ffcda23ee0c7de725ddf3102
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-13 21:13:51 +0530

    intro of "map" function

--- 05_iterations/05_filter.js
+++ 05_iterations/05_filter.js
@@ -13,11 +13,34 @@ const coding = ['ruby', 'js', 'c++', 'python']
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const newNums = myNums.filter( (item) => item > 5)
+// const newNums = myNums.filter( (item) => (item > 5) )
 
-console.log(newNums);               //   [ 6, 7, 8, 9 ]
+/*
+const newNums = []
+myNums.forEach( (num) => {
+    if(num > 4) {
+        newNums.push(num)
+    }
+})
+*/
+// console.log(newNums);               //   [ 6, 7, 8, 9 ]
 
 /*
   filter -> it returns array of elements which satisfied the given condition
 */
+
+
+/* map function */
+
+/* 
+map is used to perform any operation to the each  element of array 
+and return a new array after performing the action 
+*/
+
+const nums= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const newNums = nums.map( (item) => item + 10)
+
+console.log(newNums);
+console.log(nums);      // it doesnot manipulate the original array
 
