e92f79ac8a5535ed7cd0cef2d9ec473dd18f5a1d
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-13 21:19:34 +0530

    function chaining

--- 05_iterations/05_filter.js
+++ 05_iterations/05_filter.js
@@ -44,7 +44,7 @@ const nums= [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const newNums = nums
                     .map( (num) => num + 10 )
                     .map( (num) => num + 1)
-
+                    .filter( (num) => num >= 14)
 
 console.log(newNums);
 // console.log(nums);      // it doesnot manipulate the original array
