593e47ea8c1312639377d7c4e417df96dc7dbc1f
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-16 21:04:40 +0530

    access child and parent node

---
+++ 06_dom/02_basics.js
@@ -0,0 +1,20 @@
+/*  
+        --> .children - returns HTMLCollection of the children 
+        --> .children[1] - accessing second child
+
+
+        --> parent.firstElementChild - return first child of parent
+        --> .lastElementChild - return last child of parent
+*/
+
+/* 
+        --> .parentElement - return the parent of the element
+        --> .nextElementSibling - return the very next sibling element
+
+*/
+
+/* 
+        --> .childNodes - return the "NodeList" 
+
+        NodeList - Nodelist is a complex tree like structure. 
+*/
