2bdfee66fd713d6d4c0804959dad0ecb14fffb25
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-16 17:04:21 +0530

    basics of dom manipulation

--- 06_dom/01_basics.js
+++ 06_dom/01_basics.js
@@ -1,1 +1,20 @@
 // DOM (Document Object Model)
+
+// getting attribute 
+/*  
+    document.getElementById('mainheading').getAttribute('class')
+
+    // setting attribute -> .setAttribute('class', 'test')
+*/
+
+/* 
+    1- .textContent -> gives the whole content including the content that is "hidden"
+    2- .innerText -> gives the whole content except the hidden one
+
+*/
+
+/* 
+        convert HTMLCollection to array
+      -->  Array.from(html_collection-Name)
+
+*/
